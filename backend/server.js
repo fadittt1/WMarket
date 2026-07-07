4f72f5ad4116405ca40ef774b54f9c5e58fbb2dd
fadittt1 <fadibencheikh4@gmail.com>
2026-07-07 22:17:51 +0100

    add proxy

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,7 @@ if (missingEnvs.length > 0) {
 }
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === "production";
 
